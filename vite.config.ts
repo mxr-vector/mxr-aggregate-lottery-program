a069efb7264fca7ac0dd905e8752cbf9bde0cb6e
YuanJie <782353676@qq.com>
2024-05-14 14:20:30 +0800

    fix: ts报错

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,19 @@ import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
 import {resolve} from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: './',
+    build: {
+        outDir: "dist",
+        assetsDir: "assets", //指定静态资源存放路径
+        sourcemap: true, //是否构建source map 文件
+        terserOptions: {
+            // 生产环境移除console
+            compress: {
+                drop_console: true,
+                drop_debugger: true,
+            },
+        }
+    },
     plugins: [
         vue(),
         AutoImport({
