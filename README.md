d509f5ae27c7de9cafe2b53fbe6f3021ffb26f76
YuanJie <782353676@qq.com>
2024-05-14 14:37:06 +0800

    update: 采用离线部署hash

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 聚合随机抽签 v1.0
 这个项目旨在帮助用户在抽签时，能够快速、准确地获取抽签结果，适用于团建、会议等场景。最重要的是他是开源的，您可以基于本项目进行二次开发，实现自己的抽签系统。！
 技术架构主要为： node18.0.0+,vue3,vite,typescript,element-plus,canvas,vue-router
+
+gitee：<a href="https://gitee.com/WangJiaHui202144/aggregate-lottery-system">aggregate-lottery-system.com</a>
 # 一.随机轮盘
 
 ![随机轮盘.png](public/drawWheel.png)
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
+import legacyPlugin from '@vitejs/plugin-legacy'
 import {resolve} from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -26,7 +27,11 @@ export default defineConfig({
         }),
         Components({
             resolvers: [ElementPlusResolver()],
-        })],
+        }),
+        legacyPlugin({ // 不在服务器部署，直接双击启动插件
+            targets: ['Android > 39', 'Chrome >= 60', 'Safari >= 10.1', 'iOS >= 10.3', 'Firefox >= 54', 'Edge >= 15'],
+        })
+    ],
     resolve: {
         alias: {
             '@': resolve(__dirname, 'src')
